1a295245f4f092168180fb9f419d1dd726d9f41a
Loren Cerri <lorencerri@gmail.com>
2023-02-14 20:52:06 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -18,7 +18,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/plexidev/quick.db-docs`;
+export const GITHUB_EDIT_URL = `https://github.com/plexidev/quick.db-docs/tree/master`;
 
 export const COMMUNITY_INVITE_URL = `https://discord.gg/plexidev`;
 
